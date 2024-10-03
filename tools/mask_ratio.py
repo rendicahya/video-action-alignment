94105832d1a0f69982477274332b0d5a79882607
Randy Cahya Wihandika <rendicahya@gmail.com>
2024-10-03 17:59:03 +0900

    Cleaned up

--- tools/mask_ratio.py
+++ tools/mask_ratio.py
@@ -16,25 +16,8 @@ root = Path.cwd()
 dataset = conf.active.dataset
 detector = conf.active.detector
 det_confidence = conf.detect[detector].confidence
-# object_selection = conf.active.object_selection
-# mode = conf.active.mode
-# use_REPP = conf.active.use_REPP
-# relevancy_model = conf.active.relevancy.method
-# relevancy_thresh = str(conf.active.relevancy.threshold)
 n_files = conf.datasets[dataset].n_videos
-
-# method = "select" if object_selection else "detect"
-# method_dir = Path("data") / dataset / detector / method
 mask_dir = root / "data" / dataset / detector / str(det_confidence) / "detect" / "mask"
-
-# if method == "detect":
-#     mask_in_dir = method_dir / ("REPP/mask" if use_REPP else "mask")
-# elif method == "select":
-#     mask_in_dir = method_dir / mode / ("REPP/mask" if use_REPP else "mask")
-
-#     if mode == "intercutmix":
-#         mask_in_dir = mask_in_dir / relevancy_model / relevancy_thresh
-
 json_out_path = mask_dir / "ratio.json"
 
 print("Input:", mask_dir)
