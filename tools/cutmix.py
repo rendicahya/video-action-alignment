e068e18cf23847ab395f9e69832f1f7c32970ae7
Randy Cahya Wihandika <rendicahya@gmail.com>
2024-10-07 21:52:31 +0900

    Implemented a new idea!

--- tools/cutmix.py
+++ tools/cutmix.py
@@ -6,6 +6,7 @@ import mmcv
 import numpy as np
 
 sys.path.append(".")
+import json
 
 import random
 from collections import defaultdict
@@ -19,7 +20,9 @@ from config import settings as conf
 from python_video import frames_to_video
 
 
-def cutmix_fn(actor_path, scene_path, mask_bundle, scene_replace, scene_op, scene_mask):
+def cutmix_fn(
+    actor_path, scene_path, action_mask, scene_replace, scene_transform, scene_mask
+):
     if not actor_path.is_file() or not actor_path.exists():
         print("Not a file or not exists:", actor_path)
         return None
@@ -39,7 +42,7 @@ def cutmix_fn(actor_path, scene_path, mask_bundle, scene_replace, scene_op, scen
         scene_mask = np.moveaxis(scene_mask, -1, 0)
 
     for f, actor_frame in enumerate(actor_reader):
-        if f == len(mask_bundle) - 1:
+        if f == len(action_mask) - 1:
             return
 
         if scene_frame is None:
@@ -58,12 +61,12 @@ def cutmix_fn(actor_path, scene_path, mask_bundle, scene_replace, scene_op, scen
         elif scene_replace == "black":
             scene_frame[is_foreground] = 0
 
-        actor_mask = mask_bundle[f]
+        actor_mask = action_mask[f]
 
         if actor_mask is None:
             actor_mask = blank
 
-        if scene_op == "hflip":
+        if scene_transform == "hflip":
             scene_frame = cv2.flip(scene_frame, 1)
 
         actor = cv2.bitwise_and(actor_frame, actor_frame, mask=actor_mask)
@@ -82,8 +85,9 @@ def main():
     det_confidence = conf.detect[detector].confidence
     smooth_edge = conf.cutmix.smooth_edge
     scene_replace = conf.cutmix.scene.replace
-    scene_op = conf.cutmix.scene.op
-    use_REPP = conf.active.use_REPP
+    scene_transform = conf.cutmix.scene.transform
+    scene_selection_method = conf.cutmix.scene.selection.method
+    scene_selection_tolerance = conf.cutmix.scene.selection.tolerance
     multiplication = conf.cutmix.multiplication
     video_ext = conf.datasets[dataset].ext
     n_videos = conf.datasets[dataset].n_videos
@@ -100,8 +104,8 @@ def main():
         / detector
         / str(det_confidence)
         / "mix"
-        / scene_op
-        / scene_replace
+        / scene_selection_method
+        / scene_transform
     )
     out_ext = conf.cutmix.output.ext
     scene_dict = defaultdict(list)
@@ -112,23 +116,29 @@ def main():
     print("Input:", video_in_dir.relative_to(root))
     print("Mask:", mask_dir.relative_to(root))
     print("Output:", video_out_dir.relative_to(root))
-    print("Video writer:", video_writer)
-    print(f"Scene: {scene_replace} & {scene_op}")
+    print("Writer:", video_writer)
+    print("Scene selection:", scene_selection_method)
+    print("Scene transform:", scene_transform)
 
     assert_that(video_in_dir).is_directory().is_readable()
     assert_that(mask_dir).is_directory().is_readable()
-    assert_that(scene_replace).is_in("white", "black", "inpaint")
-    assert_that(scene_op).is_in("noop", "hflip")
+    assert_that(scene_selection_method).is_in("random", "area", "iou")
+    assert_that(scene_replace).is_in("noop", "white", "black", "inpaint")
+    assert_that(scene_transform).is_in("noop", "hflip")
 
     if not click.confirm("\nDo you want to continue?", show_default=True):
         exit("Aborted.")
 
     random.seed(random_seed)
 
-    with open(video_in_dir / "list.txt") as f:
-        for line in f:
-            action, filename = line.split()[0].split("/")
-            scene_dict[action].append(filename)
+    if scene_selection_method == "random":
+        with open(video_in_dir / "list.txt") as f:
+            for line in f:
+                action, filename = line.split()[0].split("/")
+                scene_dict[action].append(filename)
+    elif scene_selection_method == "area":
+        with open(mask_dir / "ratio.json") as f:
+            ratio_json = json.load(f)
 
     bar = tqdm(total=n_videos * multiplication, dynamic_ncols=True)
     n_written = 0
@@ -140,18 +150,41 @@ def main():
         if not video_mask_path.is_file() or not video_mask_path.exists():
             continue
 
-        mask_bundle = np.load(video_mask_path)["arr_0"]
+        action_mask = np.load(video_mask_path)["arr_0"]
         fps = mmcv.VideoReader(str(file)).fps
-        scene_action_options = [s for s in scene_dict.keys() if s != action]
         i = 0
 
-        while i < multiplication:
-            scene_action_pick = random.choice(scene_action_options)
-            video_out_path = (
-                video_out_dir / action / f"{file.stem}-{scene_action_pick}"
-            ).with_suffix(out_ext)
+        if scene_selection_method == "random":
+            scene_class_options = [s for s in scene_dict.keys() if s != action]
+        if scene_selection_method == "area":
+            action_mask_ratio = np.count_nonzero(action_mask) / action_mask.size
 
-            scene_action_options.remove(scene_action_pick)
+        while i < multiplication:
+            if scene_selection_method == "random":
+                scene_class = random.choice(scene_class_options)
+                scene_options = scene_dict[scene_class]
+                scene_pick = random.choice(scene_options)
+
+                scene_class_options.remove(scene_class)
+            elif scene_selection_method == "area":
+                mask_ratio_lower = action_mask_ratio * (1 - scene_selection_tolerance)
+                mask_ratio_upper = action_mask_ratio * (1 + scene_selection_tolerance)
+                scene_options = [
+                    filename
+                    for filename, ratio in ratio_json.items()
+                    if mask_ratio_lower < ratio < mask_ratio_upper
+                    and filename.split("_")[1] != action
+                ]
+
+                if len(scene_options) <= 1:
+                    continue
+
+                scene_pick = random.choice(scene_options) + video_ext
+                scene_class = scene_pick.split("_")[1]
+
+            video_out_path = (
+                video_out_dir / action / f"{file.stem}-{scene_class}"
+            ).with_suffix(out_ext)
 
             if (
                 video_out_path.exists()
@@ -160,24 +193,24 @@ def main():
                 bar.update(1)
                 continue
 
-            scene_options = scene_dict[scene_action_pick]
-            scene_pick = random.choice(scene_options)
-
             if scene_replace == "noop":
                 scene_mask = None
             else:
                 scene_pick_base = os.path.splitext(scene_pick)[0]
-                scene_mask_path = (
-                    mask_dir / scene_action_pick / f"{scene_pick_base}.npz"
-                )
+                scene_mask_path = mask_dir / scene_class / f"{scene_pick_base}.npz"
                 scene_mask = np.load(scene_mask_path)["arr_0"]
 
                 if len(scene_mask) > 500:
                     continue
 
-            scene_path = video_in_dir / scene_action_pick / scene_pick
+            scene_path = video_in_dir / scene_class / scene_pick
             out_frames = cutmix_fn(
-                file, scene_path, mask_bundle, scene_replace, scene_op, scene_mask
+                file,
+                scene_path,
+                action_mask,
+                scene_replace,
+                scene_transform,
+                scene_mask,
             )
 
             if out_frames:
