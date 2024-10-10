1d24ad0913f3bf1ca1c40e38c2f2067a8c68e3eb
Randy Cahya Wihandika <rendicahya@gmail.com>
2024-10-10 15:30:48 +0900

    mask_ratio.py is now multithreaded!

--- tools/mask_ratio.py
+++ tools/mask_ratio.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append(".")
 
 import json
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from pathlib import Path
 
 import click
@@ -12,28 +13,39 @@ from tqdm import tqdm
 from assertpy.assertpy import assert_that
 from config import settings as conf
 
-root = Path.cwd()
-dataset = conf.active.dataset
-detector = conf.active.detector
-det_confidence = conf.detect[detector].confidence
-n_files = conf.datasets[dataset].n_videos
-mask_dir = root / "data" / dataset / detector / str(det_confidence) / "detect" / "mask"
-json_out_path = mask_dir / "ratio.json"
+
+def calc_ratio(path):
+    mask = np.load(path)["arr_0"]
+    ratio = np.count_nonzero(mask) / mask.size
+
+    return path.stem, round(ratio, 4)
+
+
+ROOT = Path.cwd()
+DATASET = conf.active.dataset
+DETECTOR = conf.active.detector
+DET_CONFIDENCE = conf.detect[DETECTOR].confidence
+N_FILES = conf.datasets[DATASET].n_videos
+MASK_DIR = ROOT / "data" / DATASET / DETECTOR / str(DET_CONFIDENCE) / "detect" / "mask"
+MAX_WORKERS = conf.active.max_workers
+OUT_PATH = MASK_DIR / "ratio.json"
 data = {}
 
-print("Input:", mask_dir)
-print("Output:", json_out_path)
-print("n videos:", n_files)
+print("Input:", MASK_DIR)
+print("Output:", OUT_PATH)
+print("n videos:", N_FILES)
 
 if not click.confirm("\nDo you want to continue?", show_default=True):
     exit("Aborted.")
 
-assert_that(mask_dir).is_directory().is_readable()
+assert_that(MASK_DIR).is_directory().is_readable()
 
-for path in tqdm(mask_dir.glob("**/*.npz"), dynamic_ncols=True):
-    mask = np.load(path)["arr_0"]
-    ratio = np.count_nonzero(mask) / mask.size
-    data[path.stem] = round(ratio, 4)
+with ThreadPoolExecutor(max_workers=MAX_WORKERS) as exec:
+    jobs = {exec.submit(calc_ratio, path): path for path in MASK_DIR.glob("**/*.npz")}
 
-with open(json_out_path, "w") as f:
+    for future in tqdm(as_completed(jobs), total=N_FILES, dynamic_ncols=True):
+        path_stem, ratio = future.result()
+        data[path_stem] = ratio
+
+with open(OUT_PATH, "w") as f:
     json.dump(data, f)
