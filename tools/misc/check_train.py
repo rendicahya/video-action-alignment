f100278a4629b1eacd0407920349ba70541c5107
Randy Cahya Wihandika <rendicahya@gmail.com>
2024-10-29 15:20:32 +0900

    Code for checking train commands for file existence

---
+++ tools/misc/check_train.py
@@ -0,0 +1,12 @@
+from os.path import exists, basename
+
+command = input("Paste your command in one line (no new lines): ").strip()
+
+for single in command.split(";"):
+    if not single.strip():
+        continue
+
+    config = single.split()[2]
+    base = basename(config)
+
+    print(base, exists(config))
