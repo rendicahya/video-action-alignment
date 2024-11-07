d11c5e6050354960e5d1c35b71b93b4e82841c5e
Randy Cahya Wihandika <rendicahya@gmail.com>
2024-11-07 20:48:22 +0900

    Moved cutmix_fn to another file

--- tools/augmentation/cutmix.py
+++ tools/augmentation/cutmix.py
@@ -12,6 +12,7 @@ import click
 import cv2
 import mmcv
 import numpy as np
+from lib_cutmix import cutmix_fn
 from tqdm import tqdm
 
 from assertpy.assertpy import assert_that
@@ -23,100 +24,13 @@ def add_suffix(path: Path, suffix: str):
     return path.parent / (path.stem + suffix)
 
 
-def cutmix_fn(
-    actor_path,
-    scene_path,
-    action_mask,
-    scene_replace,
-    scene_mask,
-    scene_transform,
-    soft_edge,
-    scene_transform_rand,
-):
-    if not actor_path.is_file() or not actor_path.exists():
-        print("Not a file or not exists:", actor_path)
-        return None
-
-    if not scene_path.is_file() or not scene_path.exists():
-        print("Not a file or not exists:", scene_path)
-        return None
-
-    actor_reader = mmcv.VideoReader(str(actor_path))
-    w, h = actor_reader.resolution
-    scene_frame = None
-    blank = np.zeros((h, w), np.uint8)
-
-    if scene_transform:
-        do_scene_transform = scene_transform_rand.random() <= scene_transform["prob"]
-
-    if scene_mask.shape[:2] != (h, w) and scene_replace in ("white", "black"):
-        scene_mask = np.moveaxis(scene_mask, 0, -1)
-        scene_mask = cv2.resize(scene_mask, dsize=(w, h))
-        scene_mask = np.moveaxis(scene_mask, -1, 0)
-
-    if soft_edge:
-        actor_mask_normal = action_mask / 255.0
-        scene_mask_normal = scene_mask / 255.0
-
-    for f, actor_frame in enumerate(actor_reader):
-        if f == len(action_mask) - 1:
-            return
-
-        if scene_frame is None:
-            scene_reader = mmcv.VideoReader(str(scene_path))
-            scene_n_frames = scene_reader.frame_cnt
-            scene_frame = scene_reader.read()
-
-        if scene_frame.shape[:2] != (h, w):
-            scene_frame = cv2.resize(scene_frame, (w, h))
-
-        if scene_replace in ("white", "black"):
-            is_foreground = scene_mask[f % scene_n_frames] == 255
-
-        if not soft_edge:
-            if scene_replace == "white":
-                scene_frame[is_foreground] = 255
-            elif scene_replace == "black":
-                scene_frame[is_foreground] = 0
-
-        actor_mask = action_mask[f]
-
-        if actor_mask is None:
-            actor_mask = blank
-
-        if scene_transform and do_scene_transform:
-            scene_frame = scene_transform["fn"](scene_frame)
-
-        if soft_edge:
-            actor_mask_3 = np.repeat(
-                np.expand_dims(actor_mask_normal[f], axis=2), 3, axis=2
-            )
-            scene_mask_3 = np.repeat(
-                np.expand_dims(1 - scene_mask_normal[f % scene_n_frames], axis=2),
-                3,
-                axis=2,
-            )
-            actor = (actor_frame * actor_mask_3).astype(np.uint8)
-            scene = (scene_frame * scene_mask_3 * (1 - actor_mask_3)).astype(np.uint8)
-        else:
-            actor = cv2.bitwise_and(actor_frame, actor_frame, mask=actor_mask)
-            scene = cv2.bitwise_and(scene_frame, scene_frame, mask=255 - actor_mask)
-
-        mix = cv2.add(actor, scene)
-
-        scene_frame = scene_reader.read()
-
-        yield cv2.cvtColor(mix, cv2.COLOR_BGR2RGB)
-
-
 def main():
     ROOT = Path.cwd()
     DATASET = conf.active.dataset
     DETECTOR = conf.active.detector
     DET_CONF = conf.detect[DETECTOR].confidence
     SOFT_EDGE_ENABLED = conf.cutmix.soft_edge.enabled
-    TEMPORAL_MORPHOLOGY_ENABLED = conf.cutmix.temporal_morphology.enabled
-    SCENE_REPLACE = conf.cutmix.scene.replace
+    TEMPORAL_MORPHOLOGY = conf.cutmix.morphology.temporal
     SCENE_TRANSFORM_ENABLED = conf.cutmix.scene.transform.enabled
     SCENE_TRANSFORM_OP = conf.cutmix.scene.transform.op
     SCENE_SELECTION_METHOD = conf.cutmix.scene.selection.method
@@ -136,12 +50,11 @@ def main():
         / SCENE_SELECTION_METHOD
     )
 
-    if TEMPORAL_MORPHOLOGY_ENABLED:
-        TEMPORAL_MORPHOLOGY_OP = conf.cutmix.temporal_morphology.op
-        MASK_DIR = add_suffix(MASK_DIR, "-" + TEMPORAL_MORPHOLOGY_OP)
-        VIDEO_OUT_DIR = add_suffix(VIDEO_OUT_DIR, "-" + TEMPORAL_MORPHOLOGY_OP)
+    if TEMPORAL_MORPHOLOGY.enabled:
+        MASK_DIR = add_suffix(MASK_DIR, "-" + TEMPORAL_MORPHOLOGY.op)
+        VIDEO_OUT_DIR = add_suffix(VIDEO_OUT_DIR, "-" + TEMPORAL_MORPHOLOGY.op)
 
-        assert_that(TEMPORAL_MORPHOLOGY_OP).is_in("dilation", "opening", "closing")
+        assert_that(TEMPORAL_MORPHOLOGY.op).is_in("dilation", "opening", "closing")
 
     if SOFT_EDGE_ENABLED:
         MASK_DIR = add_suffix(MASK_DIR, "-soft")
@@ -181,7 +94,6 @@ def main():
     assert_that(SCENE_SELECTION_METHOD).is_in(
         "random", "iou-v", "iou-m", "bao-v", "bao-m"
     )
-    assert_that(SCENE_REPLACE).is_in("noop", "white", "black", "inpaint")
     assert_that(SCENE_TRANSFORM_OP).is_in("hflip")
 
     if not click.confirm("\nDo you want to continue?", show_default=True):
@@ -197,26 +109,26 @@ def main():
     action_name2idx = {}
 
     with open(VIDEO_IN_DIR / "list.txt") as f:
-        for i, line in enumerate(f):
-            path, action_idx = line.split()
-            action, filename = path.split("/")
-            stem = os.path.splitext(filename)[0]
+        file_list = f.readlines()
 
-            if SCENE_SELECTION_METHOD == "random":
-                action2scenes[action].append(stem)
-            elif SCENE_SELECTION_METHOD in ("iou-v", "iou-m", "bao-v", "bao-m"):
-                scene2action[stem] = action
-                action_list[i] = int(action_idx)
-                idx2stem.append(stem)
+    # with open(VIDEO_IN_DIR / "list.txt") as f:
+    for i, line in enumerate(file_list):
+        path, action_idx = line.split()
+        action, filename = path.split("/")
+        stem = os.path.splitext(filename)[0]
 
-                if action not in action_name2idx:
-                    action_name2idx[action] = int(action_idx)
+        if SCENE_SELECTION_METHOD == "random":
+            action2scenes[action].append(stem)
+        elif SCENE_SELECTION_METHOD in ("iou-v", "iou-m", "bao-v", "bao-m"):
+            scene2action[stem] = action
+            action_list[i] = int(action_idx)
+            idx2stem.append(stem)
+
+            if action not in action_name2idx:
+                action_name2idx[action] = int(action_idx)
 
     bar = tqdm(total=N_VIDEOS * MULTIPLICATION, dynamic_ncols=True)
     n_written = 0
-
-    with open(VIDEO_IN_DIR / "list.txt") as f:
-        file_list = f.readlines()
 
     for file_idx, line in enumerate(file_list):
         path, action_idx = line.split()
@@ -278,18 +190,6 @@ def main():
                     # Remember the selected video
                     used_videos.append(scene_id)
 
-            # Obsolete
-            elif SCENE_SELECTION_METHOD == "iou-10":
-                used_videos = np.where(np.isin(action_list, used_actions))
-                eligible = np.setdiff1d(all_options, used_videos, assume_unique=True)
-                # Get a random scene from top-10 IoU
-                scene_id = random.choice(eligible[:10])
-                scene = idx2stem[scene_id]
-                scene_class = scene2action[scene]
-                scene_class_idx = action_name2idx[scene_class]
-
-                used_actions.append(scene_class_idx)
-
             video_out_path = (
                 VIDEO_OUT_DIR / action / f"{file.stem}-{i}-{scene_class}"
             ).with_suffix(".mp4")
@@ -307,9 +207,8 @@ def main():
             scene_path = (VIDEO_IN_DIR / scene_class / scene).with_suffix(EXT)
             out_frames = cutmix_fn(
                 file,
-                scene_path,
                 action_mask,
-                SCENE_REPLACE,
+                scene_path,
                 scene_mask,
                 scene_transform,
                 SOFT_EDGE_ENABLED,
