86a7b0aeb7f2bf25feffd7ae96816885a965bc86
Randy Cahya Wihandika <rendicahya@gmail.com>
2024-11-14 14:10:36 +0900

    Updated iou-m and bao-m and refactoring

--- tools/augmentation/mix2train.py
+++ tools/augmentation/mix2train.py
@@ -10,7 +10,7 @@ from pathlib import Path
 import click
 import mmcv
 import numpy as np
-from lib_cutmix import cutmix_fn
+from library import cutmix_fn
 from tqdm import tqdm
 
 from assertpy.assertpy import assert_that
@@ -39,6 +39,7 @@ def main(dump_path):
     DETECTOR = conf.active.detector
     DET_CONF = str(conf.detect[DETECTOR].confidence)
     MULTIPLICATION = conf.cutmix.multiplication
+    SCENE_SELECTION = conf.cutmix.scene.selection.method
     ACTION_DIR = ROOT / "data" / DATASET / "videos"
     SCENE_DIR = ROOT / "data" / DATASET / "scene"
     MASK_DIR = ROOT / "data" / DATASET / DETECTOR / DET_CONF / "detect/mask-dilation"
@@ -48,7 +49,7 @@ def main(dump_path):
     MATRIX = np.load(MATRIX_PATH)["arr_0"]
     check_value = MATRIX[-2, -1]
     WORK_DIR = ROOT / "mmaction2/work_dirs"
-    OUT_DIR = ROOT / "data" / DATASET / "mix2train"
+    OUT_DIR = ROOT / "data" / DATASET / ("mix2train-" + SCENE_SELECTION)
 
     print("n videos:", N_VIDEOS)
     print("Action:", ACTION_DIR.relative_to(ROOT))
@@ -73,43 +74,45 @@ def main(dump_path):
         exit("Aborted.")
 
     idx2stem = []
-    stem2class = {}
-    scene_class_list = np.zeros(N_VIDEOS, np.uint8)
+    stem2label = {}
+    scene_label_list = np.zeros(N_VIDEOS, np.uint8)
+    label2idx = {}
 
     with open(dump_path, "rb") as file:
         dump_data = pickle.load(file)
 
     with open(ACTION_DIR / "list.txt") as file:
         for line in file:
-            subpath, class_idx = line.split()
-            class_name, filename = subpath.split("/")
+            subpath, label_idx = line.split()
+            label, filename = subpath.split("/")
             stem = splitext(filename)[0]
 
-            stem2class[stem] = class_name
+            stem2label[stem] = label
             idx2stem.append(stem)
 
     with open(SCENE_DIR / "list.txt") as f:
         scene_file_list = f.readlines()
 
     for i, line in enumerate(scene_file_list):
-        subpath, class_idx = line.split()
-        class_name, filename = subpath.split("/")
-        scene_class_list[i] = int(class_idx)
+        subpath, label_idx = line.split()
+        label, filename = subpath.split("/")
+        scene_label_list[i] = int(label_idx)
+
+        if label not in label2idx:
+            label2idx[label] = int(label_idx)
 
     bar = tqdm(total=len(dump_data) * MULTIPLICATION, dynamic_ncols=True)
-    n_skipped = 0
     n_written = 0
-    scene_file_list = scene_file_list[: len(dump_data) * MULTIPLICATION]
 
     for scene_file_idx, line in enumerate(scene_file_list):
-        subpath, class_idx = line.split()
+        subpath, label_idx = line.split()
         info = dump_data[scene_file_idx]
 
         if info["pred_label"] != info["gt_label"]:
             bar.update(MULTIPLICATION)
             continue
 
-        scene_class, scene_file = subpath.split("/")
+        scene_label, scene_file = subpath.split("/")
         scene_stem = splitext(scene_file)[0]
         scene_path = SCENE_DIR / subpath
         fps = mmcv.VideoReader(str(scene_path)).fps
@@ -121,20 +124,22 @@ def main(dump_path):
         all_videos = np.argsort(matrix_merge)
 
         # Videos having the same action
-        same_class = np.where(np.isin(scene_class_list, [int(class_idx)]))
+        same_label = np.where(np.isin(scene_label_list, [int(label_idx)]))
 
         # Exclude from sorting
-        eligible = np.setdiff1d(all_videos, same_class, assume_unique=True)
+        eligible = np.setdiff1d(all_videos, same_label, assume_unique=True)
 
-        for _ in range(MULTIPLICATION):
+        for i in range(MULTIPLICATION):
             # Get action video with max score
             action_id = eligible[-1]
             action_stem = idx2stem[action_id]
-            action_class = stem2class[action_stem]
-            action_path = (ACTION_DIR / action_class / action_stem).with_suffix(EXT)
-            mask_path = MASK_DIR / action_class / (action_stem + ".npz")
+            action_label = stem2label[action_stem]
+            action_path = (ACTION_DIR / action_label / action_stem).with_suffix(EXT)
+            mask_path = MASK_DIR / action_label / (action_stem + ".npz")
             out_path = (
-                OUT_DIR / action_class / f"{scene_stem}-{action_stem}"
+                OUT_DIR
+                / action_label
+                / f'{scene_stem.replace("-", "")}-{action_stem.replace("-", "")}'
             ).with_suffix(".mp4")
 
             if not mask_path.exists():
@@ -145,10 +150,20 @@ def main(dump_path):
                 bar.update(1)
                 continue
 
+            if SCENE_SELECTION in ("iou-v", "bao-v"):
+                action_label_idx = label2idx[action_label]
+                videos_selected_label = np.where(
+                    np.isin(scene_label_list, [action_label_idx])
+                )
+                eligible = np.setdiff1d(
+                    eligible, videos_selected_label, assume_unique=True
+                )
+            elif SCENE_SELECTION in ("iou-m", "bao-m"):
+                eligible = np.setdiff1d(eligible, [action_id], assume_unique=True)
+
             out_path.parent.mkdir(parents=True, exist_ok=True)
 
             action_mask = np.load(mask_path)["arr_0"]
-            eligible = np.setdiff1d(eligible, [action_id], assume_unique=True)
             out_frames = cutmix_fn(action_path, action_mask, scene_path)
 
             if out_frames:
@@ -167,7 +182,6 @@ def main(dump_path):
 
     bar.close()
     print("Written videos:", n_written)
-    print("Skipped videos:", n_skipped)
 
 
 if __name__ == "__main__":
